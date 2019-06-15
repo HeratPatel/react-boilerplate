10f52f181bc8a36fd8992bda90463911ecfda7b6
Herat Patel <heratpatel1@gmail.com>
2019-06-15 23:28:06 +0530

    Added Strict mode and react-helmet-async in replacement of react-helmet

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { Provider } from "react-redux";
 import configureStore from "./redux";
 import Routes from "./routes";
-
-const store = configureStore();
+import { HelmetProvider } from "react-helmet-async";
 
 function Main() {
+  const store = configureStore();
   return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <StrictMode>
+      <Provider store={store}>
+        <HelmetProvider>
+          <Routes />
+        </HelmetProvider>
+      </Provider>
+    </StrictMode>
   );
 }
 
--- src/hocs/ReactHelmet/ReactHelment.js
+++ src/hocs/ReactHelmet/ReactHelment.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 /**
  * ReactHelmet: Dynamically loads page title and description
